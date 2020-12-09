af86efa588d15c9e9037f66d6092d751a434f816
markoliver19 <markoliverdulay3@gmail.com>
2020-12-09 14:55:35 +0800

    uploaded by markoliver

---
+++ biodata.php
@@ -0,0 +1,749 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="dulay.css">
+	<title>MARK OLIVER DULAY</title>
+	<center><h1>BIODATA</h1></center>
+	<br><br><br><br><br>	
+
+</head>
+<body>
+<style type="text/css">
+
+
+
+
+
+.dulay{
+	margin-left:25px;
+
+	outline-color: pink;
+	color: red;
+	
+	background-color: white;
+}
+
+
+
+
+.six{
+
+	margin-left: 500px;
+position: absolute;
+bottom: 1395px;
+}
+
+
+.six1{
+margin-left: 15px;
+border-color: black;
+	outline-color: green;
+	color: red;
+	
+	background-color: white;
+
+}
+
+
+
+
+
+.seven{
+
+margin-left: 500px;
+position: absolute;
+bottom: 1350px;
+}
+
+
+.seven1{
+margin-left: 40px;
+border: none;
+border-bottom: 1px solid black;
+outline: none;
+
+}
+
+
+
+
+.nine{
+
+margin-left: 500px;
+position: absolute;
+bottom: 1305px;
+}
+
+
+
+.nine1{
+margin-left: 22px;
+border: none;
+border-bottom: 1px solid black;
+outline: none;
+
+}
+
+
+.eleven{
+
+margin-left: 500px;
+position: absolute;
+bottom: 1265px;
+}
+
+
+
+.eleven1{
+margin-left: 24px;
+border: none;
+border-bottom: 1px solid black;
+outline: none;
+
+}
+
+.eighth{
+
+margin-left: 500px;
+position: absolute;
+bottom: 910px;
+}
+
+
+.eighth1{
+
+margin-left: 24px;
+border: none;
+border-bottom: 1px solid black;
+outline: none;
+}
+
+
+
+
+.twenth{
+
+margin-left: 500px;
+position: absolute;
+bottom: 870px;
+}
+
+
+.twenth1{
+
+margin-left: 24px;
+border: none;
+border-bottom: 1px solid black;
+outline: none;
+}
+
+
+
+
+.twoth{
+
+margin-left: 500px;
+position: absolute;
+bottom: 825px;
+}
+
+
+.twoth1{
+
+margin-left: 24px;
+border: none;
+border-bottom: 1px solid black;
+outline: none;
+}
+
+
+
+.one{
+	margin-left: 100px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+.two{
+	margin-left: -2px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+.three{
+	margin-left: 30px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+.four{
+	margin-left: 49px;
+
+	border-color: black;
+	outline-color: green;
+	color: red;
+	
+	background-color: white;
+
+	
+
+	
+}
+
+.five{
+	margin-left: 58px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+.eight{
+	margin-left: 58px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+.ten{
+	margin-left: 48px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+.twe{
+	margin-left: 30px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+.thirt{
+	margin-left:25px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+.fourth{
+	margin-left:90px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+.fifth{
+	margin-left:125px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+
+.sixth{
+	margin-left:20px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+
+}
+	.seventh{
+	margin-left:25px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+.nineth{
+	margin-left:17px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+.oneth{
+	margin-left:47px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+.threeth{
+	margin-left:50px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+
+.twofour{
+	margin-left:7px;
+	border: none;
+	border-bottom: 1px solid black;
+	outline: none;
+}
+
+
+.left{
+	
+margin-left: 500px;
+position: absolute;
+bottom: 415px;
+}
+
+.left1{
+	margin-left: 500px;
+position: absolute;
+bottom: 370px;
+}
+
+
+.left2{
+	margin-left: 500px;
+position: absolute;
+bottom: 325px;
+}
+
+
+.left3{
+	margin-left: 500px;
+position: absolute;
+bottom: 285px;
+}
+.year1{
+	margin-left: 500px;
+bottom: -30px;
+position: absolute;
+}
+
+.year2{
+	margin-left: 500px;
+position: absolute;
+bottom: -70px;
+}
+
+
+.year3{
+	margin-left: 500px;
+position: absolute;
+bottom: -110px;
+}
+
+
+
+.name1{
+  position: absolute;
+}
+
+.name2{
+  position: absolute;
+}
+
+.name3{
+  position: absolute;
+}
+
+
+
+.name4{
+  position: absolute;
+}
+
+.name5{
+  position: absolute;
+}
+
+.name6{
+  position: absolute;
+}
+
+
+
+.name7{
+  position: absolute;
+}
+
+.name8{
+  position: absolute;
+}
+
+.name9{
+  position: absolute;
+}
+
+
+.name10{
+  position: absolute;
+}
+
+.name11{
+  position: absolute;
+}
+
+.name12{
+  position: absolute;
+}
+
+</style>
+  <form method="POST">
+  
+  	<div >
+  	Name:
+  	<input class="one" type='text' name='NAME'>
+  	</div>
+   
+<br>
+
+  	<div >
+  		Permanent Address:
+ <input  class="two" type='text' name='PERMANENT'>
+  	</div>
+   
+ <br>
+ 
+ 	<div>
+  		Present Address:
+ <input class="three" type='text' name='PRESENT'>
+  	</div>
+
+
+ <br>
+
+
+	<div>Date of Birth:
+ <input class="four"type='DATE'  name='BIRTH'>
+  	</div>
+<br>
+	<div>Civil Status:
+ <input class="five"type='text'  name='CIVIL'>
+
+  	</div>
+
+  	<br>
+
+  	<div>Citizenship:
+ <input class="eight"type='text'  name='CITIZENSHIP'>
+
+  	</div>
+
+  	<br>
+
+
+
+	<div>
+Place of Bith:
+ <input class="ten"type='text'  name='PLACE'>
+
+  	</div>
+<br>
+
+
+Name of Father:
+ <input class="twe"type='text'  name='NAMEF'>
+
+
+<br><br>
+
+Name of Mother:
+ <input class="thirt"type='text'  name='NAMEM'>
+
+  	</div>
+
+
+
+<br><br>
+
+Address:
+ <input class="fourth"type='text'  name='ADD'>
+
+  	</div>
+
+
+
+<br><br>
+
+Language of Dialects Spoken:
+ <input class="fifth"type='text'  name='LANGUAGE'>
+
+ 
+<br><br>
+Person to be notified In Case of Emergency:
+ <input class="sixth"type='text'  name='PERSON'>
+
+ 
+
+
+<br><br><br>
+<h1>EDUCATIONAL BACKGROUND</h1>
+
+
+
+
+<br>
+
+Elementary:
+ <input class="seventh"type='text'  name='ELEM'>
+
+  	
+<br><br>
+
+
+
+
+High School:
+ <input class="nineth"type='text'  name='HIGH'>
+
+<br><br>
+
+
+College:
+ <input class="oneth"type='text'  name='COLL'>
+
+<br><br>
+
+
+Course:
+ <input class="threeth"type='text'  name='COURSE'>
+
+<br><br>
+
+
+
+Special Skills:
+ <input class="twofour"type='text'  name='SKILLS'>
+
+<br><br>
+
+
+<div class="six">
+	 Religion:
+
+<select class="six1" name="RELIGION">
+	<option>Chatolic</option>
+	<option>Protestant</option>
+	<option>Inc</option>
+	<option>Islam</option>
+	<option>Tribal</option>
+	<option>No religion</option>
+</select>
+
+
+</div>
+
+<div class="seven">
+	 Age:
+	 <input class="seven1" size="20" type='text' name='AGE' size=10>
+</div>
+  
+
+<div class="nine">
+	 Weight:
+	 <input class="nine1" size="20" type='text' name='WEIGHT' size=10>
+</div>
+
+
+  
+<div class="eleven">
+	 Height:
+	 <input class="eleven1" size="20" type='text' name='HEIGHT' size=10>
+</div>
+  
+
+
+
+<div class="eighth">
+	 Year Graduate:
+	 <input class="eighth1" size="20" type='text' name='YEAR1' size=10>
+</div>
+  
+
+
+<div class="twenth">
+	 Year Graduate:
+	 <input class="twenth1" size="20" type='text' name='YEAR2' size=10>
+</div>
+  
+<div class="twoth">
+	 Year Graduate:
+	 <input class="twoth1" size="20" type='text' name='YEAR3' size=10>
+</div>
+<br>
+
+  
+  <input  class="dulay"  type="submit" value="Submit">
+  <br><br><br>
+
+  ***********************************************************************************************************************************************************************
+
+
+</form>
+
+
+
+
+
+<?php
+
+
+echo'<h3>STUDENT INFORMATION</h3>';
+
+echo'<br><br>';
+$name = $_POST['NAME'];
+echo "<div class='name1'>Name: $name </div>";
+
+echo'<br><br>';
+
+$name = $_POST['PERMANENT'];
+echo "<div class='name2'>Permanent Address: $name</div> ";
+
+echo'<br><br>';
+
+
+$name = $_POST['PRESENT'];
+echo "<div class='name3'>Present Address: $name</div> ";
+
+echo'<br><br>';
+
+
+$name = $_POST['BIRTH'];
+echo "<div class='name4'>Date of Birth: $name</div> ";
+
+echo'<br><br>';
+
+
+
+
+$name = $_POST['CIVIL'];
+echo "<div class='name5'>Civil: $name</div> ";
+
+echo'<br><br>';
+
+
+$name = $_POST['RELIGION'];
+echo "<div class='left'>Religion: $name </div>";
+
+
+
+$name = $_POST['AGE'];
+echo "<div class='left1'>Age: $name </div>";
+
+
+$name = $_POST['CITIZENSHIP'];
+echo "<div class='name6'>Citizenship: $name</div> ";
+
+
+echo'<br><br>';
+
+
+
+$name = $_POST['WEIGHT'];
+echo "<div class='left2'>Weight: $name </div>";
+
+
+
+$name = $_POST['PLACE'];
+echo "<div class='name7'>Place of Birth: $name</div> ";
+
+
+$name = $_POST['HEIGHT'];
+echo "<div class='left3'>Height: $name </div>";
+
+echo'<br><br>';
+
+$name = $_POST['NAMEF'];
+echo "<div class='name8'>Name of Father: $name</div> ";
+
+
+echo'<br><br>';
+
+$name = $_POST['NAMEM'];
+echo "<div class='name9'>Name of Mother: $name</div> ";
+
+
+echo'<br><br>';
+
+
+$name = $_POST['ADD'];
+echo "<div class='name10'>Address: $name</div> ";
+
+echo'<br><br>';
+
+$name = $_POST['LANGUAGE'];
+
+echo "<div class='name11'>Language of Dialects Spoken: $name</div> ";
+
+echo'<br><br>';
+
+$name = $_POST['PERSON'];
+echo "<div class='name12'>Person to be notified In Case of Emergency:  $name</div>";
+echo'<br><br>';
+
+echo'<div class="name10">
+
+  **********************************************************************************************************************************************************************</div>';
+echo'<br>';
+
+echo'<h3>EDUCATIONAL BACKGROUND</h3>';
+echo'<br>';
+
+
+$name = $_POST['ELEM'];
+echo "Elementary: $name";
+
+echo'<br><br>';
+
+$name = $_POST['HIGH'];
+echo "High School: $name";
+
+echo'<br><br>';
+
+$name = $_POST['COLL'];
+echo "College: $name";
+
+echo'<br><br>';
+
+$name = $_POST['COURSE'];
+echo "Course: $name";
+
+
+echo'<br><br>';
+
+$name = $_POST['SKILLS'];
+echo "Skills: $name";
+
+$name = $_POST['YEAR1'];
+echo "<div class='year1'>Year Graduate:  $name </div>";
+
+
+
+$name = $_POST['YEAR2'];
+echo "<div class='year2'>Year Graduate:  $name </div>";
+
+
+
+
+
+$name = $_POST['YEAR3'];
+echo "<div class='year3'>Year Graduate:  $name </div>";
+
+
+?>
+
+
+
+<br><br><br><br><br><br><br><br><br><br>
+
+
+</body>
+</html>
